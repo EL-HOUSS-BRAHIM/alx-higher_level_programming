7f2f7137a3117c8bf23f9d574f834ff824c164b7
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-10-31 16:08:48 -0700

    addvanced

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -9,8 +9,9 @@ def magic_calculation(a, b, c):
     else:
         return a * b - c
 
+
 if __name__ == "__main__":
-    
+    # Test cases
     print(magic_calculation(3, 5, 7))
     print(magic_calculation(8, 6, 10))
     print(magic_calculation(5, 3, 1))
