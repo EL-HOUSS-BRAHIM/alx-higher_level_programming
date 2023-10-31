186b389d84c49a4c636374fdc1e783a40a170de4
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-10-31 16:25:47 -0700

    advanced

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
-def print_reverse_alphabet():
-    for i in range(122, 64, -1):
-        print(chr(i), end='') if i % 2 == 0 else print(chr(i - 32), end='')
-
-if name == "main":
-    print_reverse_alphabet()
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
