28b2da438e50075858c7a2738ded06e04a13ff00
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-10-31 16:14:11 -0700

    advanced

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python3
 
-def remove_char_at(s, n):
-    if n < 0 or n >= len(s):
-        return s
-
-    return s[:n] + s[n + 1:]
-
-if name == "main":
-    print(remove_char_at("Best School", 3))
-    print(remove_char_at("Chicago", 2))
-    print(remove_char_at("C is fun!", 0))
-    print(remove_char_at("School", 10))
-    print(remove_char_at("Python", -2))
+def remove_char_at(str, n):
+    if n < 0:
+        return (str)
+    return (str[:n] + str[n+1:])
