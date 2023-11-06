00b7a3db0d11beafd3efc93bfccbe4989a59df68
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-11-06 06:59:34 -0800

    test

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,4 @@
-#!/usr/bin/env python3
-
-
+#!/usr/bin/python3
 def no_c(my_string):
-    new_s = my_string.translate({ord('c'): None})
-    new_s = new_s.translate({ord('C'): None})
-    return new_s
+    new_string = my_string.translate({ord(i): None for i in 'cC'})
+    return new_string
