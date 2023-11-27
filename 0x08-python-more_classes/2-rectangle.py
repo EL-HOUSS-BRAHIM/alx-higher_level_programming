94fb95d92189d3f1f0ad1d8070f88ef34969d479
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-11-27 04:58:45 -0800

    test

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -41,9 +41,19 @@ class Rectangle:
             self.__height = value
 
     def area(self):
-        """Return the area of the rectangle."""
+        """Calculate and return the area of the rectangle."""
         return self.__width * self.__height
 
     def perimeter(self):
-        """Return the perimeter of the rectangle."""
+        """Calculate and return the perimeter of the rectangle."""
         return 2 * (self.__width + self.__height)
+
+if __name__ == "__main__":
+    my_rectangle = Rectangle(2, 4)
+    print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+
+    print("--")
+
+    my_rectangle.width = 10
+    my_rectangle.height = 3
+    print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
