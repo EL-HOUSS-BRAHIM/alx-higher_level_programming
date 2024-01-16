11f0a357e119e09f0b52fe0d2fdcfb9e28d0791b
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2024-01-16 08:32:17 -0800

    0x0D-SQL_introduction

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
 -- Print the full discription of the table named first_table
 
-SELECT column_name, data_type, character_maximum_length FROM information_schema.columns WHERE table_schema = 'hbtn_0c_0' AND table_name = 'first_table';
+SHOW CREATE TABLE first_table;
