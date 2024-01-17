bf7082128d339690e57cebd1223b3a1d057471ad
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2024-01-17 06:53:18 -0800

    0x0E-SQL_more_queries

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -4,7 +4,8 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
     id int unique auto_increment not null primary key,
-    state_id int not null foreign key references states(id),
-    name VARCHAR(256) not null
+    state_id int not null,
+    name VARCHAR(256) not null,
+    FOREIGN KEY (state_id) REFERENCES states(id)
 );
     
