f56a2cb4373d63d0dadcc40782659b797b95a014
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2024-01-17 02:25:52 -0800

    0x0D-SQL_introduction

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- lists all records with score >= 10 in the table ORDER BY DESC
-SELECT score, name FROM second_table WHERE price >= 10  ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE score >= 10  ORDER BY score DESC;
