e974f100a6b359a580b49b9a78da24e613535951
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2024-01-17 04:42:35 -0800

    0x0E-SQL_more_queries

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,5 @@
 -- create a table if not exixts and give columns named id a unique valu.
 CREATE TABLE IF NOT EXISTS unique_id (
     id int default 1 unique,
-    name VARCHAR(256),
+    name VARCHAR(256)
 );
