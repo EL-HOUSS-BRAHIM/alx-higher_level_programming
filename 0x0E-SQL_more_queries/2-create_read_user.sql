43bc263f7901b26fd5a969181e8d8443557e1bf9
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2024-01-17 04:25:24 -0800

    0x0E-SQL_more_queries

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+-- create the database and the user on the same time.
+-- give the user select privilege in the database.
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT PRIVILEGES ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
