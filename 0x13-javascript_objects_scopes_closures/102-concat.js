b6b5616ba27d62b0b02a65533b57c79bf294e909
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2024-02-13 08:13:45 -0800

    js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,29 +1,22 @@
 #!/usr/bin/node
-
 const fs = require('fs');
 
-const [, , sourceFile1, sourceFile2, destinationFile] = process.argv;
+const fileA = process.argv[2];
+const fileB = process.argv[3];
+const fileC = process.argv[4];
 
-fs.readFile(sourceFile1, 'utf8', (err, data1) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
+if (
+  fs.existsSync(fileA) &&
+fs.statSync(fileA).isFile &&
+fs.existsSync(fileB) &&
+fs.statSync(fileB).isFile &&
+fileC !== undefined
+) {
+  const fileAContent = fs.readFileSync(fileA);
+  const fileBContent = fs.readFileSync(fileB);
+  const stream = fs.createWriteStream(fileC);
 
-  fs.readFile(sourceFile2, 'utf8', (err, data2) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-
-    const concatenatedData = data1.trim() + '\n' + data2.trim();
-
-    fs.writeFile(destinationFile, concatenatedData, (err) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      console.log(`Files ${sourceFile1} and ${sourceFile2} have been concatenated and saved to ${destinationFile}`);
-    });
-  });
-});
+  stream.write(fileAContent);
+  stream.write(fileBContent);
+  stream.end();
+}
