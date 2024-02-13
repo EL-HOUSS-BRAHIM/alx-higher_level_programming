7b65090f1943f7e35897eb74f0576d183ef69e88
next-gen bross <bross.or.of@gmail.com>
2024-02-13 12:08:54 +0100

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 // When no arguments are passed, process.argv will contain two elements: the path to the Node.js executable and the path to the script itself. So, the length of process.argv will be 2.
 if (argsLength === 2) {
-    console.log("No argument");
+    console.log('No argument');
 } 
 // When one argument is passed, process.argv will contain three elements: the path to the Node.js executable, the path to the script itself, and the one additional argument. So, the length of process.argv will be 3.
 else if (argsLength === 3) {
-    console.log("Argument found");
+    console.log('Argument found');
 } 
 // When multiple arguments are passed, process.argv will contain more than three elements: the path to the Node.js executable, the path to the script itself, and multiple additional arguments. So, the length of process.argv will be greater than 3.
 else {
-    console.log("Arguments found");
+    console.log('Arguments found');
 }
 
