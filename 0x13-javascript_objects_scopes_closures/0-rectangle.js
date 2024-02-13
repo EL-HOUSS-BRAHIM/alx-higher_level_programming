3760467633ac172c657f957af19e8d1ea13f7399
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2024-02-13 03:45:04 -0800

    js

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/**
+ * Represent a rectangle.
+ */
+class Rectangle {
+}
+
+module.exports = Rectangle;
---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/**
+ * Represents a rectangle function
+ */
+class Rectangle {
+    constructor (w, h) {
+	this.width = w;
+	this.height = h;
+    }
+}
+module.exports = Rectangle;
