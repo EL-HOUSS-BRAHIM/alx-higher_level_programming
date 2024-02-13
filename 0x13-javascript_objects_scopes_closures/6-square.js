12cc73f1a8dc61b83b6e4349f40bf1820175d1b2
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2024-02-13 08:08:12 -0800

    js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -5,8 +5,7 @@
 const Square = require('./5-square');
 class SquareWithCharPrint extends Square {
   charPrint (c) {
-    if (c === undefined)
-      c = 'X';
+    if (c === undefined) { c = 'X'; }
     for (let i = 0; i < this.height; i++) {
       console.log(c.repeat(this.width));
     }
