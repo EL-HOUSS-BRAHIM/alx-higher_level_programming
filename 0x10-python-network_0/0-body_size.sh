885d612774521eda2fe0c8e8e0c7d8b53851fa79
next-gen bross <bross.or.of@gmail.com>
2024-03-02 01:15:40 +0100

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,15 +1,3 @@
-#!/usr/bin/env bash
-#sends a request to that URL, and displays the size of the body of the response
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Send a request to the provided URL and store the response in a variable
-response=$(curl -sI $1)
-
-# Extract the content length from the response headers
-content_length=$(echo "$response" | grep -i "content-length" | awk '{print $2}' | tr -d '\r')
-
-# Print the content length in bytes
-echo "$content_length"
+#!/bin/bash
+# 0. cURL body size
+curl -Is "$1" | grep 'Content-Length' | awk '{print $2}'
