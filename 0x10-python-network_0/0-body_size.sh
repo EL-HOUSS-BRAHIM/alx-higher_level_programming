9aef645ecaab9ded631d06ccdcdfa07dd2fc2543
next-gen bross <bross.or.of@gmail.com>
2024-03-02 01:26:25 +0100

    Delete 0x10-python-network_0 directory

--- 0x10-python-network_0/0-body_size.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# 0. cURL body size
-curl -Is "$1" | grep 'Content-Length' | awk '{print $2}'
--- 0x10-python-network_0/4-header.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#sends a GET request to the provided URL with the header variable X-School-User-Id set to 98 and displays the body of the response
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Send GET request with header variable X-School-User-Id set to 98
-curl -s -X GET -H "X-School-User-Id: 98" "$1"
